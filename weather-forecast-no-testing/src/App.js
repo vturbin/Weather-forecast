fe32cf48b65aec7b3373c3f72a19725b0417ed25
vturbin <v.turbin@hotmail.com>
2020-12-28 19:52:15 +0100

    created weather forecast app

---
+++ weather-forecast-no-testing/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import 'materialize-css';
+// import { Navbar, Icon } from 'react-materialize';
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ weather-forecast-no-testing/src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header = (props) => {
+    return (
+        <div className="forecast-header">
+        <h2>
+          WEATHER4YOU
+        </h2>
+        
+       </div>
+    )
+}
+
+export default Header;
